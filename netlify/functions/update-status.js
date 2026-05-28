db8b959e69f19185c4f27bc3ffc7c53883c4458b
nazavzdyua-cpu <nazavzdy.ua@gmail.com>
2026-05-28 19:57:16 +0300

    Update update-status.js

--- netlify/functions/update-status.js
+++ netlify/functions/update-status.js
@@ -1,1 +1,64 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: "Method Not Allowed" })
+    };
+  }
 
+  try {
+    const data = JSON.parse(event.body);
+
+    if (data.password !== process.env.ADMIN_PASSWORD) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({
+          success: false,
+          error: "Невірний пароль"
+        })
+      };
+    }
+
+    const SUPABASE_URL = process.env.SUPABASE_URL;
+    const SERVICE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+    const updateRes = await fetch(
+      `${SUPABASE_URL}/rest/v1/orders?id=eq.${data.orderId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "apikey": SERVICE_KEY,
+          "Authorization": `Bearer ${SERVICE_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          status: data.status
+        })
+      }
+    );
+
+    const updateText = await updateRes.text();
+
+    if (!updateRes.ok) {
+      throw new Error(updateText);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true
+      })
+    };
+
+  } catch (error) {
+    console.log("UPDATE STATUS ERROR:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        error: error.message
+      })
+    };
+  }
+};
